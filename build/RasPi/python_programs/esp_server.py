5787d7111cda21aeeb21a9bc8c60fdc2b24313a2
caymanbarber <barbercayman@gmail.com>
2020-02-02 16:45:54 -0800

    started pi server

--- build/RasPi/python_programs/esp_server.py
+++ build/RasPi/python_programs/esp_server.py
@@ -1,1 +1,22 @@
+from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
+from io import BytesIO
+##from tbd_change_csv_file import tbd_classs
 
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        print(body)
+        #send body to parser in tbd class
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'Post request recieved ')
+        self.wfile.write(response.getvalue())
+        #print(self.rfile.read(content_length))
+
+
+
+httpd = ThreadingHTTPServer(("localhost", 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
